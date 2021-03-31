7ed0e4792ab8fac8047eb9a432a4e44816d8ba18
mina bessada <48674093+mina-adibe@users.noreply.github.com>
2021-03-31 05:22:39 +0200

    set values to be edited

--- src/components/edit/edit.js
+++ src/components/edit/edit.js
@@ -2,10 +2,15 @@ import React, { useEffect } from "react";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { useForm } from "react-hook-form";
+//import { connectModal } from "redux-modal";
+//import { Button, Modal } from "react-bootstrap";
+//import PropTypes from "prop-types";
 import "./edit.scss";
-//import { Formik } from "formik";
+import Modal from "react-modal";
 //import Select from "react-select";
 //import "react-select/dist/react-select.css";
+
+Modal.setAppElement("#root");
 
 const SignupSchema = yup.object().shape({
   vehicle: yup.string().required(),
@@ -15,53 +20,65 @@ const SignupSchema = yup.object().shape({
   fueltype: yup.string().required(),
 });
 
-export default function Edit() {
-  useEffect(() => {
-    setValue("vehicle", "vehicle");
-    setValue("startdate", "startdate");
-    setValue("odometer", "odometer");
-    setValue("volume", "volume");
-    setValue("fueltype", "fueltype");
-  }, []);
+const Edit = (props) => {
+  //console.log("props >>$$$$$$$$", props?.info);
+
+  //useEffect(() => {}, []);
 
   const { register, handleSubmit, errors, setValue } = useForm({
     resolver: yupResolver(SignupSchema),
   });
+
+  setTimeout(() => {
+    setValue("vehicle", props.info[0]);
+    setValue("startdate", props.info[1]);
+    setValue("odometer", props.info[2]);
+    setValue("volume", props.info[3]);
+    setValue("fueltype", props.info[4]);
+  }, 0);
+
   const onSubmit = (data) => {
     console.log(data);
   };
-
   return (
-    <>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div>
-          <label>vehicle</label>
-          <input type="text" name="vehicle" ref={register} />
-          {errors.vehicle && <p>{errors.vehicle.message}</p>}
-        </div>
-        <div>
-          <label>start date</label>
-          <input type="text" name="startdate" ref={register} />
-          {errors.startdate && <p>{errors.startdate.message}</p>}
-        </div>
-        <div>
-          <label>Odometer</label>
-          <input type="text" name="odometer" ref={register} />
-          {errors.odometer && <p>{errors.odometer.message}</p>}
-        </div>
-        <div>
-          <label>Volume</label>
-          <input type="text" name="volume" ref={register} />
-          {errors.volume && <p>{errors.volume.message}</p>}
-        </div>
-        <div>
-          <label>Fuel Type (optional)</label>
-          <input type="text" name="fueltype" ref={register} />
-          {errors.fueltype && <p>{errors.fueltype.message}</p>}
-        </div>
+    <div>
+      <Modal isOpen={props.show} contentLabel="test-modal">
+        <h1>{props.info[2]}</h1>
 
-        <input type="submit" />
-      </form>
-    </>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div>
+            <label>vehicle</label>
+            <input type="text" name="vehicle" ref={register} />
+            {errors.vehicle && <p>{errors.vehicle.message}</p>}
+          </div>
+          <div>
+            <label>start date</label>
+            <input type="text" name="startdate" ref={register} />
+            {errors.startdate && <p>{errors.startdate.message}</p>}
+          </div>
+          <div>
+            <label>Odometer</label>
+            <input type="text" name="odometer" ref={register} />
+            {errors.odometer && <p>{errors.odometer.message}</p>}
+          </div>
+          <div>
+            <label>Volume</label>
+            <input type="text" name="volume" ref={register} />
+            {errors.volume && <p>{errors.volume.message}</p>}
+          </div>
+          <div>
+            <label>Fuel Type (optional)</label>
+            <input type="text" name="fueltype" ref={register} />
+            {errors.fueltype && <p>{errors.fueltype.message}</p>}
+          </div>
+          <button className="modal-exit-button" onClick={props.exit}>
+            cancel
+          </button>
+          <input type="submit" />
+        </form>
+      </Modal>
+    </div>
   );
-}
+};
+
+export default Edit;
