095fc0bdefa6162c311bb5564ce6c2e3e6aabf20
mina bessada <48674093+mina-adibe@users.noreply.github.com>
2021-04-01 06:41:03 +0200

    fix style

--- src/components/fuelHistory/fuelData.js
+++ src/components/fuelHistory/fuelData.js
@@ -1,74 +1,110 @@
-import React from "react";
-
-import { Icon, Menu, Table } from "semantic-ui-react";
-
-import edit from "../../assets/images/edit.svg";
+import React, { useState } from "react";
 import del from "../../assets/images/delete.svg";
-//import styles from "./fuelData.module.scss";
+import editimg from "../../assets/images/edit.svg";
+
+import "./fuelData.scss";
 import { deleteUser } from "../../redux/action";
 
 import { useDispatch, useSelector } from "react-redux";
+//import { connect } from "react-redux";
+//import { bindActionCreators } from "redux";
+//import { show } from "redux-modal";
+
+import Edit from "../edit/edit";
 
 const FuelData = () => {
   const dispatch = useDispatch();
-  const data = useSelector((state) => state.items);
-  console.log("dataaaa-fuel:", data);
+  const data = useSelector((state) => state.itemReducer.items);
+  const [showModal, setShowModal] = useState(false);
+  const [item, setItem] = useState("");
 
-  // delete item
+  // mpdal
+  const handleShowModal = (x) => {
+    setShowModal(toggleShowModal);
+    if (showModal === false) {
+      setItem(x);
+    }
+  };
+
+  const toggleShowModal = () => {
+    return {
+      showModal: setShowModal(!showModal),
+    };
+  };
 
   return (
     <>
-      <Table fixed>
-        <Table.Header>
-          <Table.Row>
-            <Table.HeaderCell>Vehicle</Table.HeaderCell>
-            <Table.HeaderCell>Time</Table.HeaderCell>
-            <Table.HeaderCell>Total Km</Table.HeaderCell>
-            <Table.HeaderCell>Volume</Table.HeaderCell>
-            <Table.HeaderCell>Cost</Table.HeaderCell>
-            <Table.HeaderCell>Action</Table.HeaderCell>
-          </Table.Row>
-        </Table.Header>
-
-        <Table.Body>
-          {data.map((item) => (
-            <Table.Row key={item.id}>
-              <Table.Cell>{item.name} </Table.Cell>
-              <Table.Cell>{item.time} </Table.Cell>
-              <Table.Cell>{item.Total_km}</Table.Cell>
-              <Table.Cell>{item.volume}</Table.Cell>
-              <Table.Cell>{item.cost}</Table.Cell>
-              <Table.Cell>
-                <i>
-                  <img alt="edit item" src={edit} />{" "}
-                </i>
-                <i onClick={() => dispatch(deleteUser(item.id))}>
-                  <img alt="delete item" src={del} />
-                </i>
-              </Table.Cell>
-            </Table.Row>
-          ))}
-        </Table.Body>
-
-        <Table.Footer>
-          <Table.Row>
-            <Table.HeaderCell colSpan="6">
-              <Menu floated="right" pagination>
-                <Menu.Item as="a" icon>
-                  <Icon name="chevron left" />
-                </Menu.Item>
-                <Menu.Item as="a">1</Menu.Item>
-                <Menu.Item as="a">2</Menu.Item>
-                <Menu.Item as="a">3</Menu.Item>
-                <Menu.Item as="a">4</Menu.Item>
-                <Menu.Item as="a" icon>
-                  <Icon name="chevron right" />
-                </Menu.Item>
-              </Menu>
-            </Table.HeaderCell>
-          </Table.Row>
-        </Table.Footer>
-      </Table>
+      <div className="maindiv">
+        <table className="content-table">
+          <thead>
+            <tr>
+              <th>Vehicle</th>
+              <th>Time</th>
+              <th>Total KM</th>
+              <th>Volume</th>
+              <th>Cost</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((item) => (
+              <tr key={item.id}>
+                <td>
+                  {item.name} <br />
+                  {item.status === "Out Of service" ? (
+                    <span style={{ color: "red" }}>{item.status}</span>
+                  ) : item.status === "In shop" ? (
+                    <span style={{ color: "#C1931B" }}>{item.status}</span>
+                  ) : item.status === "Active" ? (
+                    <span style={{ color: "green" }}>{item.status}</span>
+                  ) : (
+                    ""
+                  )}
+                </td>
+                <td>{item.time} </td>
+                <td>{item.Total_km}</td>
+                <td>{item.volume}</td>
+                <td>{item.cost}</td>
+                <td>
+                  <tc style={{ marginRight: "27px" }}>
+                    <i
+                      onClick={() =>
+                        handleShowModal([
+                          item.name,
+                          item.time,
+                          item.Total_km,
+                          item.volume,
+                          item.cost,
+                          item.id,
+                        ])
+                      }
+                    >
+                      <img
+                        alt="edit item"
+                        src={editimg}
+                        style={{ cursor: "pointer" }}
+                      />
+                    </i>
+                  </tc>
+                  <tc>
+                    <i onClick={() => dispatch(deleteUser(item.id))}>
+                      <img
+                        alt="delete item"
+                        src={del}
+                        style={{ cursor: "pointer" }}
+                      />
+                    </i>
+                  </tc>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        {/* -------------------------- */}
+        <div>
+          <Edit show={showModal} exit={handleShowModal} info={item} />
+        </div>
+      </div>
     </>
   );
 };
