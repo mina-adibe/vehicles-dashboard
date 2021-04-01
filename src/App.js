165f86f143db4ab46fd04762a8d368f970c42271
mina bessada <48674093+mina-adibe@users.noreply.github.com>
2021-04-01 07:02:49 +0200

    add component to main page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import "./App.css";
+import FuelData from "./components/fuelHistory/fuelData";
+import Navbar from "./components/navbar/navbar";
 import Sidebar from "./components/sidebar/sidebar";
 
 function App() {
   return (
-    <div>
-      <Sidebar />
-    </div>
+    <>
+      <div className="grid-container">
+        <div className="slidbar">
+          <Sidebar />
+        </div>
+        <div className="navbar">
+          <Navbar />
+        </div>
+        <div className="body">
+          <FuelData />
+        </div>
+      </div>
+    </>
   );
 }
 
